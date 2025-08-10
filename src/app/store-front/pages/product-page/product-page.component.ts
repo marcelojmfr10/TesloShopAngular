cf2d380e785be35bda872583bffc2bce95610d7c
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-08-09 18:56:40 -0600

    Mostrar productos y página de producto

--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductCarouselComponent } from "@products/components/product-carousel/product-carousel.component";
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ProductCarouselComponent],
   templateUrl: './product-page.component.html',
 })
-export class ProductPageComponent { }
+export class ProductPageComponent {
+
+  activatedRoute = inject(ActivatedRoute);
+  productService = inject(ProductsService);
+
+  productIdSlug: string = this.activatedRoute.snapshot.params['idSlug'];
+
+  productResource = rxResource({
+    request: () => ({ idSlug: this.productIdSlug }),
+    loader: ({ request }) => {
+      return this.productService.getProductByIdSlug(request.idSlug)
+    }
+  })
+
+}
